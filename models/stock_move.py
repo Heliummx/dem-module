4c81f569cc821ae1e774d67b2d48c42ec937d627
Francisco Medina <franciscomedinaag@gmail.com>
2021-06-21 15:03:07 -0500

    just send stock on automatic action

--- models/stock_move.py
+++ models/stock_move.py
@@ -24,45 +24,47 @@ class StockMoveLineInheritProductStockPriceConnector(models.Model):
             except Exception as e:
                 _logger.error("Failed to send post request to webservice, reason : %s" % e)
 
-    def _action_done(self):
-        res = super(StockMoveLineInheritProductStockPriceConnector, self)._action_done()
-        for stock_move_line in self:
-            stock_move_line.send_new_stock_data_product_connector_webserver()
-        return res
+    # ---------------------------------- This code send a post request everytime stock is changed ----------------------------------------
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        """A function like create of the original in order to track the changing of done moves qty"""
-        mls = super(StockMoveLineInheritProductStockPriceConnector, self).create(vals_list)
-        for ml, vals in izip(mls, vals_list):
-            if ml.state == 'done':
-                if ml.product_id.type == 'product':
-                    ml.send_new_stock_data_product_connector_webserver()
-        return mls
+    # def _action_done(self):
+    #     res = super(StockMoveLineInheritProductStockPriceConnector, self)._action_done()
+    #     for stock_move_line in self:
+    #         stock_move_line.send_new_stock_data_product_connector_webserver()
+    #     return res
 
-    def write(self, vals):
-        """ A write function like in the original to update products qtys in case of modification in done state of move
-        """
-        res = super(StockMoveLineInheritProductStockPriceConnector, self).write(vals)
-        triggers = [
-            ('location_id', 'stock.location'),
-            ('location_dest_id', 'stock.location'),
-            ('lot_id', 'stock.production.lot'),
-            ('package_id', 'stock.quant.package'),
-            ('result_package_id', 'stock.quant.package'),
-            ('owner_id', 'res.partner')
-        ]
-        updates = {}
-        for key, model in triggers:
-            if key in vals:
-                updates[key] = self.env[model].browse(vals[key])
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     """A function like create of the original in order to track the changing of done moves qty"""
+    #     mls = super(StockMoveLineInheritProductStockPriceConnector, self).create(vals_list)
+    #     for ml, vals in izip(mls, vals_list):
+    #         if ml.state == 'done':
+    #             if ml.product_id.type == 'product':
+    #                 ml.send_new_stock_data_product_connector_webserver()
+    #     return mls
 
-        if updates or 'qty_done' in vals:
+    # def write(self, vals):
+    #     """ A write function like in the original to update products qtys in case of modification in done state of move
+    #     """
+    #     res = super(StockMoveLineInheritProductStockPriceConnector, self).write(vals)
+    #     triggers = [
+    #         ('location_id', 'stock.location'),
+    #         ('location_dest_id', 'stock.location'),
+    #         ('lot_id', 'stock.production.lot'),
+    #         ('package_id', 'stock.quant.package'),
+    #         ('result_package_id', 'stock.quant.package'),
+    #         ('owner_id', 'res.partner')
+    #     ]
+    #     updates = {}
+    #     for key, model in triggers:
+    #         if key in vals:
+    #             updates[key] = self.env[model].browse(vals[key])
 
-            mls = self.filtered(lambda ml: ml.move_id.state == 'done' and ml.product_id.type == 'product')
-            if not updates:  # we can skip those where qty_done is already good up to UoM rounding
-                mls = mls.filtered(lambda ml: not float_is_zero(ml.qty_done - vals['qty_done'],
-                                                                precision_rounding=ml.product_uom_id.rounding))
-            for ml in mls:
-                ml.send_new_stock_data_product_connector_webserver()
-        return res
+    #     if updates or 'qty_done' in vals:
+
+    #         mls = self.filtered(lambda ml: ml.move_id.state == 'done' and ml.product_id.type == 'product')
+    #         if not updates:  # we can skip those where qty_done is already good up to UoM rounding
+    #             mls = mls.filtered(lambda ml: not float_is_zero(ml.qty_done - vals['qty_done'],
+    #                                                             precision_rounding=ml.product_uom_id.rounding))
+    #         for ml in mls:
+    #             ml.send_new_stock_data_product_connector_webserver()
+    #     return res
